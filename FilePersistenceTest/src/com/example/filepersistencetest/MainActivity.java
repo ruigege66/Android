27e34fb6f1bf9ba6db94a6701f75fd095e0e7cfd
ruigege66 <1215217867@qq.com>
2020-08-24 00:03:32 +0800

    提交从app文件中读取文字

--- FilePersistenceTest/src/com/example/filepersistencetest/MainActivity.java
+++ FilePersistenceTest/src/com/example/filepersistencetest/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.filepersistencetest;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
@@ -21,6 +26,38 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		edit = (EditText)findViewById(R.id.edit);
+		
+		String inputText = load();
+		if(!TextUtils.isEmpty(inputText)) {
+			edit.setText(inputText);
+			edit.setSelection(inputText.length());
+			Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+		}
+	}
+	public String load() {
+		FileInputStream in = null;
+		BufferedReader reader = null;
+		StringBuilder content = new StringBuilder();
+		try {
+			in = openFileInput("data");
+			reader = new BufferedReader(new InputStreamReader(in));
+			String line = "";
+			while((line = reader.readLine()) != null) {
+				content.append(line);
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(reader != null) {
+				try {
+					reader.close();
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return content.toString();
+	
 	}
 
 	@Override
