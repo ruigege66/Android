ff2dc063676b1da8101f214d795c5e43bd9813f3
ruigege66 <1215217867@qq.com>
2020-10-15 00:27:21 +0800

    提交读取联系人

--- ContactsTest/src/com/example/contactstest/MainActivity.java
+++ ContactsTest/src/com/example/contactstest/MainActivity.java
@@ -1,34 +1,51 @@
 package com.example.contactstest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	
+	ListView contactsView;
+	
+	ArrayAdapter<String> adapter;
+	
+	List<String> contactsList = new ArrayList<String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		contactsView = (ListView) findViewById(R.id.contacts_view);
+		adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contactsList);
+		readContacts();
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	
+	private void readContacts() {
+		Cursor cursor = null;
+		try {
+			//查询联系人
+			cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+			while(cursor.moveToNext()) {
+				//获取联系人
+				String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+				String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+				contactsList.add(displayName + "\n" + number);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(cursor != null) {
+				cursor.close();
+			}
 		}
-		return super.onOptionsItemSelected(item);
 	}
 }
