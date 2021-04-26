612e6bb35e00a273ad269e56893e2efd0f352f69
dongqianrui <1215217867@qq.com>
2021-04-27 00:22:21 +0800

    Update newFile.java

--- newFile.java
+++ newFile.java
@@ -3,6 +3,10 @@ public  class Test {
     public static void main(String[] args) {
         
         System.out.println("youxiu");
+        
         return ;
+    }
+    public void test1() {
+        
     }
 }
